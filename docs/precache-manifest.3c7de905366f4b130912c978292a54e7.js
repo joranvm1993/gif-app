f67833f8eab5cd9e8c9e822fdcfd9c030706400e
Andres Vasquez <joranvm93@gmail.com>
2020-12-04 23:39:44 -0500

    first commit

---
+++ docs/precache-manifest.3c7de905366f4b130912c978292a54e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f65e9b09de4c52ef1c902f2e55fd4a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f2f92097e7718e4560c",
+    "url": "/static/css/main.aaa3e196.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2f92097e7718e4560c",
+    "url": "/static/js/main.0bf2473f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import { getGifs } from '../helpers/getGifs'
+
+
+export const useFetchGifs = ( category ) => {
+    
+    const [state, setState] = useState({
+        data: [],
+        loading: true 
+    });
+
+    useEffect(() => {
+
+        getGifs(category)
+            .then( imgs => {
+               
+                setTimeout(() => {
+                    
+                    setState ({
+                        data: imgs,
+                        loading: false
+                    });
+
+                }, 3000);
+
+
+            }) 
+
+    }, [category])
+
+       // useEffect(() => {
+    //     getGifs( category )
+    //         .then( imgs => setImages( imgs ) );
+    // }, [ category ])
+
+ 
+
+    return state;
+    
+}
