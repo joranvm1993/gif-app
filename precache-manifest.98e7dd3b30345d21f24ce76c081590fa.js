4bd40f2cd2822fbf9f5c9b7cd9980ed0a5056881
Andres Vasquez <joranvm93@gmail.com>
2020-12-10 01:47:51 -0500

    Updates

---
+++ precache-manifest.98e7dd3b30345d21f24ce76c081590fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d9b9f8c374074af60eb78627c1c19f",
+    "url": "/gif-app/index.html"
+  },
+  {
+    "revision": "6f2f92097e7718e4560c",
+    "url": "/gif-app/static/css/main.aaa3e196.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/gif-app/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gif-app/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f2f92097e7718e4560c",
+    "url": "/gif-app/static/js/main.0bf2473f.chunk.js"
+  },
+  {
+    "revision": "f027d27279aeba6be3bb",
+    "url": "/gif-app/static/js/runtime-main.a544f01e.js"
+  }
+]);
